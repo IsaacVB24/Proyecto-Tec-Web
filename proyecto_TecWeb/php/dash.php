d1c83779b6cae1c0764460c00f09a0ffbbc005d2
Isaac Victoria <isaac127@live.com.mx>
2022-06-16 10:47:15 -0500

    Versión inestable de dash.php

--- proyecto_TecWeb/php/dash.php
+++ proyecto_TecWeb/php/dash.php
@@ -1,8 +1,4 @@
 <?php
-    echo "Hola Mundo desde PHP";
-    $var = 1;
-    $var = $var + 1;
-    echo $var;
 ?>
 
 <!DOCTYPE html>
@@ -10,18 +6,19 @@
 <head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<title>Inicio</title>
+<title>Dashboard</title>
 <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'/>
 <meta name="description" content="">
 <meta name="keywords" content="">
-<link href="../css/estilos.css" rel="stylesheet">
+<link href="../css/dash.css" rel="stylesheet">
 <link rel="icon" type="image/x-icon" href="../imagenesProyecto_TecWeb/logo_escom2.png">
-<script type="text/javascript" src="../javaScript/despliegue.js"></script>
 <script type="text/javascript" src="../javaScript/encabezadoYPiePagina.js"></script>
 </head>
 <body onload="hF()">
     <div id="header" class="general">
-    </div><div id="footer" class="general">
+    </div>
+    
+    <div id="footer" class="general">
     </div>
 </body>
 </html>
