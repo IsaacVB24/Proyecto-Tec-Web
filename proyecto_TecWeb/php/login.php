b793e02f333d1922b4ea9544d71c54d8d90c8528
Isaac Victoria <isaac127@live.com.mx>
2022-06-16 09:32:32 -0500

    Primer prueba de funcionamiento php

--- proyecto_TecWeb/php/login.php
+++ proyecto_TecWeb/php/login.php
@@ -1,3 +1,20 @@
 <?php
     echo "Hola Mundo";
 ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<title>Dashboard</title>
+<meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'/>
+<meta name="description" content="">
+<meta name="keywords" content="">
+<link href="" rel="stylesheet">
+</head>
+<body>
+    
+</body>
+</html>
