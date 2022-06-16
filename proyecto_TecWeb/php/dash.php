2359b3d0290cd23648d5a0c2707b903a756a23ca
Isaac Victoria <isaac127@live.com.mx>
2022-06-16 12:08:29 -0500

    Actualización de dash.php

--- proyecto_TecWeb/php/dash.php
+++ proyecto_TecWeb/php/dash.php
@@ -8,29 +8,29 @@
     if(!$conexion){
         die("Conexión fallida: " . $conexion->connect_error);
     }else{
-        // Código
+?>              
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Dashboard</title>
+        <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'/>
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <link href="../css/dash.css" rel="stylesheet">
+        <link href="../css/estilos.css" rel="stylesheet">
+        <link rel="icon" type="image/x-icon" href="../imagenesProyecto_TecWeb/logo_escom2.png">
+        <script type="text/javascript" src="../javaScript/encabezadoYPiePagina.js"></script>
+        </head>
+        <body onload="hF()">
+            <div id="header" class="general">
+            </div>
+            
+            <div id="footer" class="general">
+            </div>
+        </body>
+        </html>
+<?php
     }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<title>Dashboard</title>
-<meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'/>
-<meta name="description" content="">
-<meta name="keywords" content="">
-<link href="../css/dash.css" rel="stylesheet">
-<link href="../css/estilos.css" rel="stylesheet">
-<link rel="icon" type="image/x-icon" href="../imagenesProyecto_TecWeb/logo_escom2.png">
-<script type="text/javascript" src="../javaScript/encabezadoYPiePagina.js"></script>
-</head>
-<body onload="hF()">
-    <div id="header" class="general">
-    </div>
-    
-    <div id="footer" class="general">
-    </div>
-</body>
-</html>
