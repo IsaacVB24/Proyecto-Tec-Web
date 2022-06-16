41d64602efb20655baf1c6aadae665c788ca3291
Isaac Victoria <isaac127@live.com.mx>
2022-06-16 10:37:18 -0500

    Pruebas en login.php

--- proyecto_TecWeb/php/login.php
+++ proyecto_TecWeb/php/login.php
@@ -1,20 +1,27 @@
 <?php
     echo "Hola Mundo desde PHP";
+    $var = 1;
+    $var = $var + 1;
+    echo $var;
 ?>
-
 
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<title>Dashboard</title>
+<title>Inicio</title>
 <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'/>
 <meta name="description" content="">
 <meta name="keywords" content="">
-<link href="" rel="stylesheet">
+<link href="../css/estilos.css" rel="stylesheet">
+<link rel="icon" type="image/x-icon" href="../imagenesProyecto_TecWeb/logo_escom2.png">
+<script type="text/javascript" src="../javaScript/despliegue.js"></script>
+<script type="text/javascript" src="../javaScript/encabezadoYPiePagina.js"></script>
 </head>
-<body>
-    Hola mundo desde HTML
+<body onload="hF()">
+    <div id="header" class="general">
+    </div><div id="footer" class="general">
+    </div>
 </body>
 </html>
